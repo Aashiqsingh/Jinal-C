b1ba6b3b61e804b11616909054296e2f7f627d1b
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-06 12:14:36 +0530

    square

---
+++ Do while loop/Pattern/square.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main(){
+
+    int i = 1;
+
+
+    do{
+        int j = 1;
+
+        do{
+            printf(" *");
+            j++;
+        }while(j <= 5);
+
+        printf("\n");
+
+        i++;
+    }while(i <= 5);
+
+
+
+
+
+
+    // do{
+    //     printf("*\n");
+    //     i++;
+    // }while(i<=5);
+
+}
