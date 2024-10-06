c4c803da2dbb8d3b35d89658dc5971ef6be0f7b7
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-06 11:30:24 +0530

    do while loop

---
+++ Do while loop/first.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+void main(){
+
+
+
+
+    int i = 0;
+
+    do{
+        // printf("%d\n",i);
+        if(i%2==1){
+            printf("%d\n",i);
+        }
+        i++;
+
+    }while(i<=5);
+
+
+
+
+
+
+    
+
+
+    // int i=1;
+
+    // do{
+    //     printf("Hello world\n");
+
+    //     i++;
+    // }while(i<=5);
+
+
+
+
+
+    // Exit controlled loop
+
+    // In do while loop program will be execute at least one time 
+
+    // int i = 5; // initialization
+
+    // do{
+    //     // printf("") Execution 
+
+    //     // i++; increment / decrement
+    // }while(condition)
+    
+}
